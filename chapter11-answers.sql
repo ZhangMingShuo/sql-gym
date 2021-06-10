85201ea33e581b3afc82a18b5f67939ff0c21e00
ZhangMingShuo <zhangms97@163.com>
2021-06-10 14:38:37 +0800

    检索订购BR01产品的客户

--- chapter11-answers.sql
+++ chapter11-answers.sql
@@ -7,4 +7,42 @@ WHERE cust_id in (
          Orders
     WHERE OrderItems.order_num = Orders.order_num
       and item_price >= 10
-)
+);
+
+# 2.何人何时订购BR01
+
+SELECT Orders.cust_id,Orders.order_date
+FROM OrderItems,Orders
+WHERE prod_id='BR01' and orderitems.order_num = Orders.order_num
+ORDER BY order_date DESC;
+
+# 3.
+SELECT Orders.cust_id,Orders.order_date
+FROM OrderItems,Orders
+WHERE prod_id='BR01' and orderitems.order_num = Orders.order_num
+ORDER BY order_date DESC;
+
+SELECT cust_id
+FROM Orders
+WHERE order_num in (
+    SELECT order_num
+    FROM Orderitems
+    WHERE prod_id = 'BR01'
+);
+
+# 4.
+SELECT cust_email,cust_name
+FROM Customers
+WHERE cust_id in (
+    SELECT cust_id
+    FROM Orders
+    WHERE order_num in (
+        SELECT order_num
+        FROM Orderitems
+        WHERE prod_id='BR01'
+        )
+    );
+
+
+
+
